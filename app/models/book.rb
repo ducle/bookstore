d882ae1408ff978c8eb47c38f2e37fcb8bda3375
Ducle <ducle.sg@gmail.com>
2018-02-23 09:16:05 +0700

    add friendly id

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+  extend FriendlyId
+  
+  belongs_to :category
+
+  validates :title, presence: true
+
+  friendly_id :title, use: :slugged
+  mount_uploader :image, ImageUploader
 end
