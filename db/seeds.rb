80a220e9461f851a8d92279d481edcc2028a667b
Ducle <ducle.sg@gmail.com>
2018-02-23 14:21:36 +0700

    manage books and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+{
+  'IT books' => [
+    'Computers',
+    'Database',
+    'Network',
+    'Web development',
+    'Mobile development',
+  ],
+  'Business' => [
+    'Startup',
+    'Eco system',
+    'Macro economics',
+    'Micro economics',
+    'Management'
+  ],
+  'Languages' => [
+    'English',
+    'Chinese',
+    'Vietnamese',
+    'Japanese',
+    'French'
+  ]
+}.each do |k, v|
+  root_cat = Category.create!(name: k)
+  v.each do |ct|
+    Category.create!(name: ct, parent_id: root_cat.id)
+  end
+end
